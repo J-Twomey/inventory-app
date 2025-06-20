f6ad0c413e31096a6ccb0cfa16dfed5c66ef99db
J-Twomey <jackpetertwomey@gmail.com>
2025-06-20 14:52:32 +0900

    trailing comma

--- src/database.py
+++ src/database.py
@@ -12,7 +12,7 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./test2.db'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={'check_same_thread': False}
+    connect_args={'check_same_thread': False},
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
