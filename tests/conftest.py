8cc0a5955fd0fd353809dbe1ad9abafa4fb69cbc
J-Twomey <jackpetertwomey@gmail.com>
2025-06-20 14:52:52 +0900

    add tests for some crud functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+
+from datetime import date
 from typing import Generator
 
 import pytest
@@ -9,11 +11,13 @@ from sqlalchemy.orm import (
     sessionmaker,
 )
 
+import src.item_enums as item_enums
 from main import app
 from src.database import (
     Base,
     get_db,
 )
+from src.schemas import ItemCreate
 
 
 TEST_DATABASE_URL = 'sqlite:///:memory:'
@@ -48,3 +52,68 @@ def client(db_session: Session) -> Generator[TestClient, None, None]:
     app.dependency_overrides[get_db] = override_get_db
     with TestClient(app) as cl:
         yield cl
+
+
+class ItemFactory:
+    def get(
+        self,
+        name: str = 'TESTER',
+        set_name: str = 'Base Set',
+        category: item_enums.Category = item_enums.Category.CARD,
+        language: item_enums.Language = item_enums.Language.KOREAN,
+        qualifiers: list[item_enums.Qualifier] = [],
+        details: str | None = None,
+        purchase_date: date = date(2025, 5, 5),
+        purchase_price: int = 1000,
+        status: item_enums.Status = item_enums.Status.STORAGE,
+        intent: item_enums.Intent = item_enums.Intent.SELL,
+        grading_fee: dict[int, int] = {},
+        grade: float | None = None,
+        grading_company: item_enums.GradingCompany = item_enums.GradingCompany.RAW,
+        cert: int | None = None,
+        submission_number: list[int] = [],
+        list_price: float | None = None,
+        list_type: item_enums.ListingType = item_enums.ListingType.NO_LIST,
+        list_date: date | None = None,
+        sale_total: float | None = None,
+        sale_date: date | None = None,
+        shipping: float | None = None,
+        sale_fee: float | None = None,
+        usd_to_jpy_rate: float | None = None,
+        group_discount: bool = False,
+        object_variant: item_enums.ObjectVariant = item_enums.ObjectVariant.STANDARD,
+        audit_target: bool = False,
+    ) -> ItemCreate:
+        return ItemCreate(
+            name=name,
+            set_name=set_name,
+            category=category,
+            language=language,
+            qualifiers=qualifiers,
+            details=details,
+            purchase_date=purchase_date,
+            purchase_price=purchase_price,
+            status=status,
+            intent=intent,
+            grading_fee=grading_fee,
+            grade=grade,
+            grading_company=grading_company,
+            cert=cert,
+            submission_number=submission_number,
+            list_price=list_price,
+            list_type=list_type,
+            list_date=list_date,
+            sale_total=sale_total,
+            sale_date=sale_date,
+            shipping=shipping,
+            sale_fee=sale_fee,
+            usd_to_jpy_rate=usd_to_jpy_rate,
+            group_discount=group_discount,
+            object_variant=object_variant,
+            audit_target=audit_target,
+        )
+
+
+@pytest.fixture(scope='function')
+def item_factory() -> ItemFactory:
+    return ItemFactory()
--- tests/crud_test.py
+++ tests/crud_test.py
@@ -4,21 +4,26 @@ from sqlalchemy.orm import Session
 
 import src.crud as crud
 import src.item_enums as item_enums
-import src.schemas as schemas
+from .conftest import ItemFactory
 
 
-def test_create_item(db_session: Session) -> None:
-    item = schemas.ItemCreate(
-        name='TESTER',
-        set_name='Base Set',
-        category=item_enums.Category.CARD,
-        language=item_enums.Language.KOREAN,
+def test_create_item_all_nullables_none(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item = item_factory.get()
+    db_item = crud.create_item(db_session, item)
+    assert db_item.id is not None
+    assert db_item.name == 'TESTER'
+
+
+def test_create_item_all_nullables_provided(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item = item_factory.get(
         qualifiers=[item_enums.Qualifier.UNLIMITED],
         details='Mint',
-        purchase_date=date(2025, 5, 5),
-        purchase_price=1000,
-        status=item_enums.Status.CLOSED,
-        intent=item_enums.Intent.SELL,
         grading_fee={1: 600, 2: 399},
         grade=10.,
         grading_company=item_enums.GradingCompany.PSA,
@@ -32,11 +37,133 @@ def test_create_item(db_session: Session) -> None:
         shipping=25.,
         sale_fee=25.,
         usd_to_jpy_rate=150.0,
-        group_discount=False,
-        object_variant=item_enums.ObjectVariant.STANDARD,
-        audit_target=False,
     )
     db_item = crud.create_item(db_session, item)
     assert db_item.id is not None
     assert db_item.name == 'TESTER'
     assert db_item.grading_fee_total == 999
+
+
+def test_get_item_returns_correct_item(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item = item_factory.get(name='GET_ITEM_TEST_ITEM')
+    db_item = crud.create_item(db_session, item)
+    result = crud.get_item(db_session, db_item.id)
+    assert result is not None
+    assert result.id == db_item.id
+    assert result.name == 'GET_ITEM_TEST_ITEM'
+
+
+def test_get_item_no_item(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item = item_factory.get(name='GET_ITEM_TEST_NO_ITEM')
+    db_item = crud.create_item(db_session, item)
+    result = crud.get_item(db_session, db_item.id + 1)
+    assert result is None
+
+
+def test_get_items_return_all_items(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item_names = ['one', 'two', 'three']
+    items = [item_factory.get(n) for n in item_names]
+    ids = []
+    for item in items:
+        db_item = crud.create_item(db_session, item)
+        ids.append(db_item.id)
+    result = crud.get_newest_items(db_session, skip=0, limit=100)
+    result_ids = [r.id for r in result]
+    result_names = [r.name for r in result]
+    assert len(result) == len(item_names)
+    assert result_ids == ids
+    assert result_names == item_names
+
+
+def test_get_items_skip_newest_item(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item_names = ['one', 'two', 'three']
+    items = [item_factory.get(n) for n in item_names]
+    ids = []
+    for item in items:
+        db_item = crud.create_item(db_session, item)
+        ids.append(db_item.id)
+    result = crud.get_newest_items(db_session, skip=1, limit=100)
+    result_ids = [r.id for r in result]
+    result_names = [r.name for r in result]
+    assert len(result) == len(item_names) - 1
+    assert result_ids == ids[:-1]
+    assert result_names == item_names[:-1]
+
+
+def test_get_items_take_two_items(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item_names = ['one', 'two', 'three']
+    items = [item_factory.get(n) for n in item_names]
+    ids = []
+    for item in items:
+        db_item = crud.create_item(db_session, item)
+        ids.append(db_item.id)
+    result = crud.get_newest_items(db_session, skip=0, limit=2)
+    result_ids = [r.id for r in result]
+    result_names = [r.name for r in result]
+    assert len(result) == len(item_names) - 1
+    assert result_ids == ids[1:]
+    assert result_names == item_names[1:]
+
+
+def test_get_items_skip_all_items(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item_names = ['one', 'two', 'three']
+    items = [item_factory.get(n) for n in item_names]
+    for item in items:
+        crud.create_item(db_session, item)
+    result = crud.get_newest_items(db_session, skip=3, limit=100)
+    assert len(result) == 0
+
+
+def test_get_items_no_items(db_session: Session) -> None:
+    result = crud.get_newest_items(db_session, skip=0, limit=100)
+    assert len(result) == 0
+
+
+def test_delete_item_by_id_success(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item_names = ['one', 'two']
+    items = [item_factory.get(n) for n in item_names]
+    ids = []
+    for item in items:
+        db_item = crud.create_item(db_session, item)
+        ids.append(db_item.id)
+    result = crud.delete_item_by_id(db_session, ids[-1])
+    assert result is True
+    assert crud.get_item(db_session, ids[-1]) is None
+    assert crud.get_item(db_session, ids[0]) is not None
+
+
+def test_delete_item_by_id_failure(
+        db_session: Session,
+        item_factory: ItemFactory,
+) -> None:
+    item_names = ['one', 'two']
+    items = [item_factory.get(n) for n in item_names]
+    ids = []
+    for item in items:
+        db_item = crud.create_item(db_session, item)
+        ids.append(db_item.id)
+    result = crud.delete_item_by_id(db_session, ids[-1] + 1)
+    assert result is False
+    for id in ids:
+        assert crud.get_item(db_session, id) is not None
