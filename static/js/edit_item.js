3c1597caba45dec4b46b1e5f849a3a973d80eb32
J-Twomey <jackpetertwomey@gmail.com>
2025-08-12 10:42:28 +0900

    2 space indentation for js

--- static/js/edit_item.js
+++ static/js/edit_item.js
@@ -4,12 +4,12 @@ import { getOriginalFormValues, disableUnchangedFields } from './form_utils.js';
 document.getElementById('grading-fees-container').addEventListener('input', addNewGradingFeeRow);
 
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    if (!form) return;
+  const form = document.querySelector('form');
+  if (!form) return;
 
-    const originalValues = getOriginalFormValues(form);
+  const originalValues = getOriginalFormValues(form);
 
-    form.addEventListener('submit', () => {
-        disableUnchangedFields(form, originalValues);
-    });
+  form.addEventListener('submit', () => {
+    disableUnchangedFields(form, originalValues);
+  });
 });
