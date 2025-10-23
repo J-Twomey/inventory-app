04bc28199cbb24f74b8f273a3365cf422ea41974
J-Twomey <jackpetertwomey@gmail.com>
2025-10-23 15:51:51 +0900

    add default purchase date and remove some unused js code

--- static/js/add_item.js
+++ static/js/add_item.js
@@ -1,3 +1,14 @@
-import { addNewGradingFeeRow } from './grading_fees_container.js';
+document.addEventListener('DOMContentLoaded', () => {
+  const purchaseDateInput = document.getElementById('purchase_date');
+  const today = new Date().toISOString().split('T')[0];
+  purchaseDateInput.value = today;
 
-document.getElementById('grading-fees-container').addEventListener('input', addNewGradingFeeRow);
+  const changeDateBy = (days) => {
+    const currentDate = new Date(dateInput.value);
+    currentDate.setDate(currentDate.getDate() + days);
+    dateInput.value = currentDate.toISOString().split('T')[0];
+  };
+
+  document.getElementById('date-decrease').addEventListener('click', () => changeDateBy(-1));
+  document.getElementById('date-increase').addEventListener('click', () => changeDateBy(1));
+});
